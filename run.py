1b9709789ab11b7af2ba4068cfc3984ada09b5ba
august-work <154587234+august-unsainted@users.noreply.github.com>
2025-08-26 09:20:44 +0800

    Добавление первого блока

---
+++ run.py
@@ -0,0 +1,21 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from handlers import start
+
+from config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+async def main():
+    dp.include_routers(start.router, start.bot_config.router)
+    await dp.start_polling(bot, skip_updates=True)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Бот выключен')
